7255594ef60fc38245974021cecaac6b1318c775
Fyodor Wolf <fwolf@bu.edu>
2013-04-12 17:09:37 -0400

    focus on textedit removed.

--- src/com/fyodorwolf/studybudy/MainActivity.java
+++ src/com/fyodorwolf/studybudy/MainActivity.java
@@ -1,11 +1,8 @@
 package com.fyodorwolf.studybudy;
 
-import java.util.List;
-
 import android.app.ActionBar;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
-import android.content.AsyncQueryHandler;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -25,6 +22,7 @@ public class MainActivity extends ListActivity{
 
 	EditText searchBox;
 	ListView listView;
+	Cursor dataProvider;
 	
 	private static final String TAG = "MainActivity";
 	
@@ -45,27 +43,31 @@ public class MainActivity extends ListActivity{
         //define the local views to be used
     	searchBox = (EditText) this.findViewById(R.id.edit_text);
     	listView = (ListView)this.getListView();
-
+        
     	/*
     	 *Add local view listeners 
     	****************************/
         searchBox.addTextChangedListener(new TextWatcher() {
 
 			@Override
-			public void afterTextChanged(Editable s) {
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 				// TODO Auto-generated method stub
+				
 			}
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
+			@Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 //				_myAdapter.getFilter().filter(s);
             	Log.d(TAG,"Search: "+s.toString());
             }
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+			}
         });
-        
+
+    	listView.requestFocus();
     	listView.setAdapter(new SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,null,null,null,CursorAdapter.NO_SELECTION));
         listView.setOnItemClickListener(new OnItemClickListener(){
-
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view, int position,long id) {
 				Log.d(TAG,"Clicked "+id);
@@ -84,16 +86,17 @@ public class MainActivity extends ListActivity{
         return true;
     }
     
-    /*****************************
-     * 		Private Classes		 *
-     *****************************/
+    
+    
+/********************************************************************************************************************************************
+ * 							Private Classes		 																							*
+ ********************************************************************************************************************************************/
 	private class SelectDataTask extends AsyncTask<String, Integer, Cursor> {
 		
-		private final String TAG = "SelectDataTask";
 		private final DatabaseAdapter myDB = DatabaseAdapter.getInstance(MainActivity.this);
 		private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
 
-		// can use UI thread here
+		@Override
 		protected void onPreExecute() {
 			this.dialog.setMessage("Selecting data...");
 			this.dialog.show();
@@ -103,15 +106,9 @@ public class MainActivity extends ListActivity{
 		protected Cursor doInBackground(String... params) {
 			return myDB.getCursor(params[0]);
 		}
-		// can use UI thread here
+		
+		@Override
 		protected void onPostExecute(final Cursor result) {
-			Log.d(TAG,result.toString());
-			result.moveToFirst();
-			while(result.getPosition() != result.getCount()){
-				Log.d(TAG,"name"+result.getPosition()+": "+result.getString(1));
-				result.moveToNext();
-			}
-			result.moveToFirst();
 	    	SimpleCursorAdapter adp = (SimpleCursorAdapter) listView.getAdapter();
 	    	adp.changeCursorAndColumns(result, new String[]{"name"},new int[]{android.R.id.text1});
 			this.dialog.hide();
